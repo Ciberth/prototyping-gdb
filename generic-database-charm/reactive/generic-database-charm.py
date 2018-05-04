706e446cfa4b11b1e105e7222524bcc8d635d5b0
Ciberth <thomasclauwaert@gmail.com>
2018-05-04 11:19:38 +0200

    not bugfree see existing db + master keyerror

--- generic-database-charm/reactive/generic-database-charm.py
+++ generic-database-charm/reactive/generic-database-charm.py
@@ -6,6 +6,8 @@ from charmhelpers.core.hookenv import log, status_set, config
 from charmhelpers.core.templating import render
 from charms.reactive import when, when_not, set_flag, clear_flag, when_file_changed
 
+concrete_config = {}
+
 @when('apache.available')
 def finishing_up_setting_up_sites():
     host.service_reload('apache2')
@@ -22,13 +24,24 @@ def ready():
 @when('pgsqldb.connected', 'gdb.pgsql.requested')
 def request_db(pgsql):
     pgsql.set_database('mygdb_first')
+    concrete_config['technology'] = "postgresql"
     status_set('maintenance', 'requesting pgsql db')
 
 
 @when('pgsqldb.master.available', 'gdb.pgsql.requested')
 def render_pgsql_config(pgsql):
+    concrete_config['master'] = pgsql.master['master']
+    concrete_config['user'] = pgsql.master['user']
+    concrete_config['password'] = pgsql.master['password']
+    concrete_config['port'] = pgsql.master['port']
+    concrete_config['dbname'] = pgsql.master['dbname']
+    
     render('gdb-config.j2', '/var/www/generic-database-charm/gdb-config.html', {
         'db_master': pgsql.master,
+        'db_pass': pgsql.master['password'],
+        'db_host': pgsql.master['dbname'],
+        'db_user': pgsql.master['user'],
+        'db_port': pgsql.master['port'],
     })
     clear_flag('gdb.pgsql.requested')
     set_flag('gdb.pgsql.available')
@@ -37,9 +50,14 @@ def render_pgsql_config(pgsql):
 # todo ? config changed
 
 @when('pgsqldb.master.available', 'gdb.pgsql.available')
-def new_incoming_relation(pgsql):
-    render('gdb-config.j2', '/var/www/generic-database-charm/gdb-config.html', {
-        'db_master': pgsql.master,
+def new_incoming_relation():
+    # share details to new consumer
+    render('gdb-config.j2', '/var/www/generic-database-charm/gdb-second.html', {
+        'db_master': concrete_config['master'],
+        'db_pass': concrete_config['password'],
+        'db_host': concrete_config['dbname'],
+        'db_user': concrete_config['user'],
+        'db_port': concrete_config['port'],
     })
     set_flag('restart-app')
 
